fadc14ab3b6857d6be66f5a6d6eb1898df9592fa
Fernando Constantino <const.fernando@gmail.com>
2023-10-09 22:36:15 -0300

    feat: developing the menu (#29)
    * feat: developing the menu
    
    * chore: review fix
    
    * chore: review fix

---
+++ services/menu.service.ts
@@ -0,0 +1,23 @@
+import { MenuItem } from '~/types/menu-item.type'
+import { MenuList } from '~/types/meny-list.type'
+
+const items: MenuList = {
+  1: {
+    icon: 'mdi:mdi-apps',
+    title: 'Feed',
+    path: '/',
+  },
+  2: {
+    icon: 'mdi:mdi-chart-bubble',
+    title: 'Material Rodante',
+    path: '/inspire',
+  },
+}
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+export const getAll = async (): Promise<MenuItem[]> => {
+  await sleep(10)
+
+  return Object.values(items)
+}
